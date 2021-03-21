2b207414b3b95eee43508d776a5fdbee36c4f2d9
zhucj <zhucj@servyou.com.cn>
2021-03-21 09:06:20 +0800

    TooLongFrameException

--- codec/src/main/java/io/netty/handler/codec/TooLongFrameException.java
+++ codec/src/main/java/io/netty/handler/codec/TooLongFrameException.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012 The Netty Project
- *
- * The Netty Project licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
 package io.netty.handler.codec;
 
 /**
@@ -20,6 +5,17 @@ package io.netty.handler.codec;
  * decoded is greater than the allowed maximum.
  */
 public class TooLongFrameException extends DecoderException {
+
+    /**
+     * 由于 Netty 是一个异步框架，所以需要在字节可以解码之前在内存中缓冲它们。因此，不能 让解码器缓冲大量的数据以至于耗尽可用的内存。
+     *
+     * 为了解除这个常见的顾虑，Netty 提供了 TooLongFrameException 类，其将由解码器在帧超出指定的大小限制时抛出。
+     *
+     * 为了避免这种情况，你可以设置一个最大字节数的阈值，如果超出该阈值，则会导致抛出一 个 TooLongFrameException
+     * (随后会被 ChannelHandler.exceptionCaught()方法捕 获)。
+     *
+     * 然后，如何处理该异常则完全取决于该解码器的用户。
+     */
 
     private static final long serialVersionUID = -1995801950698951640L;
 
