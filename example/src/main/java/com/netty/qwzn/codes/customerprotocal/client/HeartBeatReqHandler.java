de8fcb6ee651d51c79e7ea70cf8a1a8f1c9fae4a
zhucj <zhucj@servyou.com.cn>
2021-03-27 20:50:57 +0800

    HeartBeatReqHandler

---
+++ example/src/main/java/com/netty/qwzn/codes/customerprotocal/client/HeartBeatReqHandler.java
@@ -0,0 +1,66 @@
+package com.netty.qwzn.codes.customerprotocal.client;
+
+import com.netty.qwzn.codes.customerprotocal.Header;
+import com.netty.qwzn.codes.customerprotocal.MessageType;
+import com.netty.qwzn.codes.customerprotocal.NettyMessage;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Lilinfeng
+ * @version 1.0
+ * @date 2014年3月15日
+ */
+public class HeartBeatReqHandler extends SimpleChannelInboundHandler<NettyMessage> {
+
+    private volatile ScheduledFuture<?> heartBeat;
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, NettyMessage message) throws Exception {
+        // 握手成功，主动发送心跳消息
+        if (message.getHeader() != null && message.getHeader().getType() == MessageType.LOGIN_RESP.value()) {
+            heartBeat = ctx.executor().scheduleAtFixedRate(new HeartBeatTask(ctx), 0, 5000, TimeUnit.MILLISECONDS);
+        } else if (message.getHeader() != null && message.getHeader().getType() == MessageType.HEARTBEAT_RESP.value()) {
+            System.out.println("Client receive server heart beat message : ---> " + message);
+        } else {
+            ctx.fireChannelRead(message);
+        }
+    }
+
+    private static class HeartBeatTask implements Runnable {
+
+        private final ChannelHandlerContext ctx;
+
+        public HeartBeatTask(final ChannelHandlerContext ctx) {
+            this.ctx = ctx;
+        }
+
+        @Override
+        public void run() {
+            NettyMessage heatBeat = buildHeatBeat();
+            System.out.println("Client send heart beat messsage to server : ---> " + heatBeat);
+            ctx.writeAndFlush(heatBeat);
+        }
+
+        private NettyMessage buildHeatBeat() {
+            NettyMessage message = new NettyMessage();
+            Header header = new Header();
+            header.setType(MessageType.HEARTBEAT_REQ.value());
+            message.setHeader(header);
+            return message;
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        if (heartBeat != null) {
+            heartBeat.cancel(true);
+            heartBeat = null;
+        }
+        ctx.fireExceptionCaught(cause);
+    }
+}
---
+++ example/src/main/java/com/netty/qwzn/codes/customerprotocal/client/LoginAuthReqHandler.java
@@ -0,0 +1,59 @@
+package com.netty.qwzn.codes.customerprotocal.client;
+
+import com.netty.qwzn.codes.customerprotocal.Header;
+import com.netty.qwzn.codes.customerprotocal.MessageType;
+import com.netty.qwzn.codes.customerprotocal.NettyMessage;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * @author Lilinfeng
+ * @version 1.0
+ * @date 2014年3月15日
+ */
+public class LoginAuthReqHandler extends SimpleChannelInboundHandler<NettyMessage> {
+
+    /**
+     * Calls {@link ChannelHandlerContext#fireChannelActive()} to forward to the
+     * next {@link ChannelHandler} in the {@link ChannelPipeline}.
+     *
+     * Sub-classes may override this method to change behavior.
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(buildLoginReq());
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, NettyMessage message) throws Exception {
+
+        // 如果是握手应答消息，需要判断是否认证成功
+        if (message.getHeader() != null && message.getHeader().getType() == MessageType.LOGIN_RESP.value()) {
+            byte loginResult = (byte) message.getBody();
+            if (loginResult != (byte) 0) {
+                // 握手失败，关闭连接
+                ctx.close();
+            } else {
+                System.out.println("Login is ok : " + message);
+                ctx.fireChannelRead(message);
+            }
+        } else {
+            ctx.fireChannelRead(message);
+        }
+    }
+
+    private NettyMessage buildLoginReq() {
+        NettyMessage message = new NettyMessage();
+        Header header = new Header();
+        header.setType(MessageType.LOGIN_REQ.value());
+        message.setHeader(header);
+        return message;
+    }
+
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+            throws Exception {
+        ctx.fireExceptionCaught(cause);
+    }
+}
